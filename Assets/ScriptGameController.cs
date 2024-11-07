5ebab11712f02744825d3719d1885e03edbaf806
Chuong <chuongshinichi@gmail.com>
2024-11-07 23:18:51 +0700

    ok

--- Assets/ScriptGameController.cs
+++ Assets/ScriptGameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
+//using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
@@ -10,7 +10,7 @@ public class ScriptGameController : MonoBehaviour
 {
     private GameObject[] buttonObject;
     private Button[,] button;
-    private Image[,] image;
+    private Text[,] player;
 
     int a = 1, b = 1;
 
@@ -28,14 +28,17 @@ public class ScriptGameController : MonoBehaviour
                 z++;
             }
 
-        image = new Image[3, 3];
+        player = new Text[3, 3];
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
             {
-                image[i, j] = button[i, j].transform.Find("Image").GetComponent<Image>();
+                player[i, j] = button[i, j].transform.Find("Text").GetComponent<Text>();
             }
 
-
+        foreach (Button btn in button)
+        {
+            btn.onClick.AddListener(SetTurn);
+        }
 
     }
     // Start is called before the first frame update
@@ -46,28 +49,54 @@ public class ScriptGameController : MonoBehaviour
 
     public void SetTurn()
     {
+        GameObject clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
         TurnOffAllCurrentButton();// tắt ảnh, tắt nút
 
-        image[a, b].enabled = false;
-        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+        if (clickedButton != null)
+        {
+            Button clickedButtonComponent = clickedButton.GetComponent<Button>();
 
-        for (int i = 0; i < 3; i++)
+            if (clickedButtonComponent != null)
+            {
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (button[i, j] == clickedButtonComponent)
+                        {
+                            a = i; b = j;
+                        }
+                    }
+
+                Debug.Log($"{a}   {b}");
+            }
+            else
+            {
+                Debug.LogError("Clicked object does not have a Button component.");
+            }
+        }
+        else
+        {
+            Debug.LogError("No button was clicked.");
+        }
+
+        //GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+
+        /*for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
             {
-                if (button[i, j] = clickedButton.GetComponent<Button>())
+                if (button[i, j] == clickedButton.GetComponent<Button>())
                 {
                     a = i; b = j;
                 }
-            }
+            }*/
 
-        Debug.Log($"{a}   {b}");
-
+        
         EndTurn();// bật ảnh bật nút
     }
 
     void EndTurn() // sau khi xác định được a,b thì hàm này để bật ảnh, bật nút
     {
-        image[a, b].enabled = true;
+        player[a, b].text = "X";
         if (a + 1 < 3) button[a + 1, b].interactable = true;
         if (a - 1 > -1) button[a - 1, b].interactable = true;
         if (b + 1 < 3) button[a, b + 1].interactable = true;
@@ -76,11 +105,11 @@ public class ScriptGameController : MonoBehaviour
 
     void TurnOffAllCurrentButton()// Hàm này để tắt ảnh, nút trước khi di chuyển
     {
-        image[a, b].enabled = false;
+        player[a, b].text = "";
         if (a + 1 < 3) button[a + 1, b].interactable = false;
         if (a - 1 > -1) button[a - 1, b].interactable = false;
         if (b + 1 < 3) button[a, b + 1].interactable = false;
         if (b - 1 > -1) button[a, b - 1].interactable = false;
-    }    
+    }
 
 }
