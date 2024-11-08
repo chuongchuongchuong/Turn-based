c404831b5476289607b0ba74f6b32fd4734c1ec3
Chuong <chuongshinichi@gmail.com>
2024-11-08 15:27:56 +0700

    oke

--- Assets/ScriptGameController.cs
+++ Assets/ScriptGameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-//using UnityEngine.EventSystems;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
@@ -12,25 +12,29 @@ public class ScriptGameController : MonoBehaviour
     private Button[,] button;
     private Text[,] player;
 
-    int a = 1, b = 1;
+    int a1 = 0, b1 = 0;// vị trí của O1: góc trên bên trái
+    int a2 = 0, b2 = 6;// vị trí của O2:góc trên bên phải
+    int a3 = 6, b3 = 0;// vị trí của O3: góc dưới bên trái
+    int a4 = 6, b4 = 6;// vị trí của O4: góc dưới bên phải
+    int a = 3, b = 3;// vị trí của X ở giữa bản đồ
 
 
     private void Awake()
     {
         buttonObject = GameObject.FindGameObjectsWithTag("button");
 
-        button = new Button[3, 3];
+        button = new Button[7, 7];
         int z = 0;
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 0; i < 7; i++)
+            for (int j = 0; j < 7; j++)
             {
                 button[i, j] = buttonObject[z].GetComponent<Button>();
                 z++;
             }
 
-        player = new Text[3, 3];
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+        player = new Text[7, 7];
+        for (int i = 0; i < 7; i++)
+            for (int j = 0; j < 7; j++)
             {
                 player[i, j] = button[i, j].transform.Find("Text").GetComponent<Text>();
             }
@@ -49,10 +53,10 @@ public class ScriptGameController : MonoBehaviour
 
     public void SetTurn()
     {
-        GameObject clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
-        TurnOffAllCurrentButton();// tắt ảnh, tắt nút
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+        TurnOffAllCurrentButton();
 
-        if (clickedButton != null)
+        if (clickedButton != null)  // vong lặp này để xác định lại vị trí a,b mới
         {
             Button clickedButtonComponent = clickedButton.GetComponent<Button>();
 
@@ -79,31 +83,20 @@ public class ScriptGameController : MonoBehaviour
             Debug.LogError("No button was clicked.");
         }
 
-        //GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
-
-        /*for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-            {
-                if (button[i, j] == clickedButton.GetComponent<Button>())
-                {
-                    a = i; b = j;
-                }
-            }*/
-
-        
         EndTurn();// bật ảnh bật nút
     }
 
-    void EndTurn() // sau khi xác định được a,b thì hàm này để bật ảnh, bật nút
+    void EndTurn() // bật hết các nút ở các vị trí X, O
     {
-        player[a, b].text = "X";
-        if (a + 1 < 3) button[a + 1, b].interactable = true;
-        if (a - 1 > -1) button[a - 1, b].interactable = true;
-        if (b + 1 < 3) button[a, b + 1].interactable = true;
-        if (b - 1 > -1) button[a, b - 1].interactable = true;
+        button[a1,b1].interactable = true;
+        button[a2,b2].interactable = true;
+        button[a3,b3].interactable = true;
+        button[a4,b4].interactable = true;
+        button[a,b].interactable = true;
+        
     }
 
-    void TurnOffAllCurrentButton()// Hàm này để tắt ảnh, nút trước khi di chuyển
+    void TurnOffAllCurrentButton()// Hàm này để nút và tạo tường
     {
         player[a, b].text = "";
         if (a + 1 < 3) button[a + 1, b].interactable = false;
